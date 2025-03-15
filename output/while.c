363d8eacfdb342396802e8c6993ac7c750d85166
Hk1250 <hildohenrik088@gmail.com>
2025-03-15 00:46:49 +0000

    tema 3

--- output/while.c
+++ output/while.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 int main () {
     
-int i= 1; 
+int i= 0; 
 
 //while (i <= 10)
 //{
@@ -16,9 +16,9 @@ while (i <= 10)
     if (i % 2 == 0)
     {
         printf("%d \n", i);
-        i++;
+       
     }
-    
+    i++;
 
 }
 
